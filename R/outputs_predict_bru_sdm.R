8629bb6736ca39806c549112d8b3c2c9b47f04bd
Philip Mostert <philism@dhcp-10-24-23-217.wlan.ntnu.no>
2021-11-03 09:33:35 +0100

    Fixed predicting with shared spatial and intercept

--- R/outputs_predict_bru_sdm.R
+++ R/outputs_predict_bru_sdm.R
@@ -106,7 +106,7 @@ predict.bru_sdm <- function(object, data = NULL, formula = NULL, mesh = NULL,
       
   if (intercept) {
         
-  if (!paste0(datasetstopredict[[i]],'_intercept')%in%row.names(object$summary.fixed) | !'intercept'%in%row.names(object$summary.fixed)) stop('Either dataset name is incorrect or bru_sdm model run without intercepts.')
+  if (!paste0(datasetstopredict[[i]],'_intercept')%in%row.names(object$summary.fixed) & !'intercept'%in%row.names(object$summary.fixed)) stop('Either dataset name is incorrect or bru_sdm model run without intercepts.')
   else intercept_obj <- paste0(datasetstopredict[[i]],'_intercept')
         
   } 
