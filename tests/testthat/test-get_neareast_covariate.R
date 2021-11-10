e6000d6890c3d844270aa12430aac528fb23c910
Philip Mostert <philism@dhcp-10-24-21-201.wlan.ntnu.no>
2021-11-10 15:29:13 +0100

    Added testthat for get_nearest_covariate

---
+++ tests/testthat/test-get_neareast_covariate.R
@@ -0,0 +1,26 @@
+test_that('Test that get_nearest_covariate returns non-null covariates at each data point', {
+  
+  ##Use data from PointedSDMs
+ library(PointedSDMs)
+  
+  Projection <- CRS("+proj=longlat +ellps=WGS84")
+  data("SolTin_covariates")
+  covariates <- sp::SpatialPointsDataFrame(coords = SolTin_covariates[,c('X','Y')],
+                                           data = SolTin_covariates[,c('Forest', 'NPP','Altitude')],
+                                           proj4string = Projection) 
+  data("SolTin_ebird")
+  ebird <- sp::SpatialPoints(SolTin_ebird[,c("X","Y")], proj4string = Projection)
+  
+  nearest_cov <- get_nearest_covariate(points = ebird,
+                                       spatialcovariates = covariates,
+                                       covariatestokeep = c('Forest','NPP'),
+                                       coords = c('X','Y'),
+                                       proj = Projection,
+                                       componentstokeep = c('POresp','PAresp','weights'))
+  
+  expect_equal(class(nearest_cov)[1], 'SpatialPointsDataFrame')
+  expect_equal(names(nearest_cov), c('Forest','NPP'))
+  expect_equal(any(sapply(nearest_cov@data, is.na)), FALSE)
+  
+  
+})
