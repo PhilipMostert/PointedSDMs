0209be5fdd45c15db250117d7e3e84a209a8d2a2
Philip Mostert <philism@dhcp-10-24-22-246.wlan.ntnu.no>
2021-11-15 12:34:53 +0100

    Changes in print for leave one out to reflect left out dataset.

--- R/outputs_leave_one_out.R
+++ R/outputs_leave_one_out.R
@@ -16,7 +16,7 @@ print.bru_sdm_leave_one_out <- function(x, ...) {
   
   for (name in 1:length(x)) {
     
-    cat('Changes in fixed values by leaving out', paste0(names(attributes(x)$differences)[name],':'))
+    cat('Changes in fixed values by leaving out', gsub("Leaving_out_.*?","",paste0(names(attributes(x)$differences)[name]),':'))
     cat('\n\n')
     
     
