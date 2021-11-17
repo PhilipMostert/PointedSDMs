34e472a71fcd96d4012acf034e890c4ca8a82b98
Philip Mostert <philism@dhcp-10-24-21-192.wlan.ntnu.no>
2021-11-17 09:46:35 +0100

    First commit test-leave_one_out

---
+++ tests/testthat/test-leave_one_out.R
@@ -0,0 +1,47 @@
+
+
+testthat::test_that('Test that leave_one out works properly.', {
+  
+  ##Use data from PointedSDMs
+  library(PointedSDMs)
+  library(spatstat)
+  library(sp)
+  
+  Projection <- CRS("+proj=longlat +ellps=WGS84")
+  Meshpars <- list(cutoff=0.8, max.edge=c(1, 3), offset=c(1,1))
+  
+  ebird <- PointedSDMs::SolTin_ebird
+  parks <- PointedSDMs::SolTin_parks
+  
+  #Add species
+  ebird$species <- 'ebird_species'
+  parks$species <- 'parks_species'
+  
+  region.mask=as.owin(cbind(SolTin_covariates[,c("X","Y")], In=rep(TRUE,nrow(SolTin_covariates))),
+                      step=c(0.25, 0.3))
+  region.mask$m[is.na(region.mask$m)] <- FALSE
+  Region.poly <- simplify.owin(as.polygonal(region.mask), dmin=0.5)
+  PolyPoints <- cbind(Region.poly$bdry[[1]]$x[c(1,length(Region.poly$bdry[[1]]$x):1)],
+                      Region.poly$bdry[[1]]$y[c(1,length(Region.poly$bdry[[1]]$y):1)])
+  Pgon <- Polygons(list(region=Polygon(coords=PolyPoints)), ID="region")
+  region.polygon=SpatialPolygons(list(Pgon), proj4string = Projection)
+  
+  data_to_use <- organize_data(ebird, parks, poresp = 'poresp', paresp = 'Present',
+                               coords = c('X','Y'), proj = Projection,
+                               boundary = region.polygon, meshpars = Meshpars,
+                               speciesname = 'species')
+  
+  integrated_model <- bru_sdm(data_to_use, 
+                              spatialcovariates = SolTin_covariates, 
+                              tolerance = 0.2)
+
+  ##Test leaving out ebird dataset
+  ebird_out <- leave_one_out(model = integrated_model, dataset = 'ebird',
+                             predictions = TRUE)
+  
+  ##
+  expect_false('ebird'%in%names(ebird_out$Leaving_out_ebird$dataset_names))
+  expect_length(ebird_out$Leaving_out_ebird$.args$control.family, 1)
+  
+    
+})
