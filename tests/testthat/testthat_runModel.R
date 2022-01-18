d3b61b77f39f802b780e78688efc40530a4d0db4
Philip Mostert <philism@dhcp-10-24-20-62.wlan.ntnu.no>
2022-01-18 14:48:31 +0100

    First commit for tests of runModel.

---
+++ tests/testthat/testthat_runModel.R
@@ -0,0 +1,93 @@
+test_that('runModel runs a dataSDM object, and produces an INLA model with extra metadata.', {
+  
+  ##Set up a model
+  ##Set up arbitrary data
+  projection <- CRS('+proj=tmerc')
+  x <- c(16.48438,  17.49512,  24.74609, 22.59277, 16.48438)
+  y <- c(59.736328125, 55.1220703125, 55.0341796875, 61.142578125, 59.736328125)
+  xy <- cbind(x, y)
+  
+  Poly = Polygon(xy)
+  Poly = Polygons(list(Poly),1)
+  SpatialPoly = SpatialPolygons(list(Poly), proj4string = projection)
+  
+  ##Old coordinate names
+  #Make random points
+  #Random presence only dataset
+  PO <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
+  ##Add random variable
+  PO$numvar <- runif(n = nrow(PO@coords))
+  PO$factvar <- sample(x = c('a','b'), size = nrow(PO@coords), replace = TRUE)
+  PO$species <- sample(x = c('fish'), size = nrow(PO@coords), replace = TRUE)
+  #Random presence absence dataset
+  PA <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
+  PA$PAresp <- sample(x = c(0,1), size = nrow(PA@coords), replace = TRUE)
+  #Add trial name
+  PA$trial <- sample(x = c(1,2,3), size = nrow(PA@coords), replace = TRUE)
+  PA$pointcov <- runif(n = nrow(PA@coords))
+  PA$binommark <- sample(x = 2:3, size = nrow(PA@data), replace = TRUE)
+  PA$marktrial <- sample(x = 3:5, size = nrow(PA@data), replace = TRUE)
+  PA$species <- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
+  mesh <- PointedSDMs::MakeSpatialRegion(bdry = SpatialPoly, meshpars = list(max.edge = 2))
+  mesh <- mesh$mesh
+  iPoints <- inlabru::ipoints(samplers = SpatialPoly)
+  ##Make PA a data.frame object
+  PA <- data.frame(PA)
+  
+  coordnames <- colnames(PO@coords)
+  responseCounts <- 'count'
+  responsePA <- 'PAresp'
+  trialName <- 'trial'
+  markNames <- c('numvar', 'factvar', 'binommark')
+  marksFamily <- c('gaussian', 'multinomial', 'binomial')
+  markTrial = 'marktrial'
+  pointCovs <- 'pointcov'
+  speciesName <- 'species'
+  
+  cov <- sp::spsample(x = SpatialPoly, n = 1000, type = 'stratified')
+  cov$covariate <- rgamma(n = nrow(cov@coords), shape = 2)
+  cov <- sp::SpatialPixelsDataFrame(points = cov@coords,
+                                    data = data.frame(covariate = cov$covariate),
+                                    proj4string = projection,
+                                    tolerance = 0.585235)
+  cov <- raster(cov)
+  
+  
+  obj <- bruSDM(PO, PA, Coordinates = coordnames, Projection = projection, INLAmesh = mesh,
+                IPS = iPoints, trialsPA = trialName, responseCounts = responseCounts, 
+                responsePA = responsePA, markNames = NULL, markFamily = NULL,
+                speciesName = speciesName, spatialCovariates = cov)
+  
+  ##run model
+  spatMod <- runModel(data = obj, options  = list(control.inla=list(int.strategy='eb')))
+  
+  expect_setequal(class(spatMod), c('bruSDM', 'bru', 'iinla', 'inla'))
+  expect_setequal(row.names(spatMod$summary.fixed), c( "fish_covariate", "bird_covariate",'fish_intercept', 'bird_intercept'))
+  expect_equal(spatMod[['species']][['speciesVar']], 'species')
+  expect_equal(spatMod[['species']][['speciesIn']][['PO']], 'fish')
+  expect_equal(spatMod[['species']][['speciesIn']][['PA']], 'bird')
+  
+  expect_equal(deparse1(spatMod[['componentsJoint']]), "~-1 + shared_spatial(main = coordinates, model = spdeModel) + species_spatial(main = coordinates, model = speciesModel, group = species, ngroup = 2) + fish_covariate(main = fish_covariate, model = \"linear\") + bird_covariate(main = bird_covariate, model = \"linear\") + fish_intercept(1) + bird_intercept(1)")
+  
+  
+  expect_output(summary(spatMod), 'Summary for fish:')
+  expect_output(summary(spatMod), 'Summary for bird:')
+  
+  expect_equal(spatMod[['spatCovs']][['name']], 'covariate')
+  expect_equal(spatMod[['spatCovs']][['class']], c(covariate = 'numeric'))
+  
+  ##Run with marks
+  obj2 <- bruSDM(PO, PA, Coordinates = coordnames, Projection = projection, INLAmesh = mesh,
+                IPS = iPoints, trialsPA = trialName, responseCounts = responseCounts, 
+                responsePA = responsePA, markNames = c('factvar', 'binommark'), markFamily = c('multinomial', 'binomial'),
+                speciesName = speciesName, spatialCovariates = cov, trialsMarks = 'marktrial')
+
+  spatMod2 <- runModel(data = obj2,
+                       options  = list(control.inla=list(int.strategy='eb')))
+  
+  expect_output(summary(spatMod2), 'Summary of multinomial variables:')
+  expect_true(spatMod2[['multinomVars']] == 'factvar')
+  expect_setequal(spatMod2[['source']], c('PO', 'PO', 'PA', 'PA'))
+  expect_setequal(sapply(spatMod2$.args$control.family, function(x) x$link), c('log', 'log', 'cloglog', 'cloglog'))
+  
+  })
