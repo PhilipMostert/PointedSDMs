fac89ec04f031b1e321e90dd069f1a52d4b54bb4
Philip Mostert <philism@dhcp-10-24-21-173.wlan.ntnu.no>
2022-02-20 10:05:23 +0100

    removed the spatialCovariates test that

--- tests/testthat/testthat_dataSDM.R
+++ tests/testthat/testthat_dataSDM.R
@@ -17,6 +17,7 @@ PO <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
 PO$numvar <- runif(n = nrow(PO@coords))
 PO$factvar <- sample(x = c('a','b'), size = nrow(PO@coords), replace = TRUE)
 PO$species <- sample(x = c('fish'), size = nrow(PO@coords), replace = TRUE)
+PO$temp <- sample(x = c(1,2), size = nrow(PO@coords), replace = TRUE)
 #Random presence absence dataset
 PA <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
 PA$PAresp <- sample(x = c(0,1), size = nrow(PA@coords), replace = TRUE)
@@ -26,6 +27,7 @@ PA$pointcov <- runif(n = nrow(PA@coords))
 PA$binommark <- sample(x = 2:5, size = nrow(PA@data), replace = TRUE)
 PA$marktrial <- sample(x = 0:1, size = nrow(PA@data), replace = TRUE)
 PA$species <- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
+PA$temp <- sample(x = c(1,2), size = nrow(PA@coords), replace = TRUE)
 mesh <- PointedSDMs::MakeSpatialRegion(bdry = SpatialPoly, meshpars = list(max.edge = 2))
 mesh <- mesh$mesh
 iPoints <- inlabru::ipoints(samplers = SpatialPoly)
@@ -44,6 +46,8 @@ spatCovs <- NULL #for now
 speciesName <- 'species'
 markSpatial <- TRUE
 marksIntercept <- TRUE
+temporalName <- 'temp'
+temporalModel <- deparse(list(model = 'ar1'))
 
 test_that('dataSDMs initialize works as expected.', {
   
@@ -62,8 +66,8 @@ test_that('dataSDMs initialize works as expected.', {
                        spatialcovariates = spatCovs,
                        speciesname = speciesName,
                        ips = iPoints,
-                       spatial = TRUE,
-                       intercepts = TRUE)
+                       spatial = TRUE, temporal = temporalName, 
+                       intercepts = TRUE, temporalmodel = temporalModel)
   ##Test private classes
   expect_true(all(check$.__enclos_env__$private$Coordinates%in%coordnames))
   expect_true(identical(check$.__enclos_env__$private$Projection,projection))
@@ -79,6 +83,7 @@ test_that('dataSDMs initialize works as expected.', {
   expect_true(check$.__enclos_env__$private$Spatial)
   expect_true(check$.__enclos_env__$private$Intercepts)
   
+  expect_equal(check$.__enclos_env__$private$temporalName, 'temp')
   #Remove one of the response variable names
   expect_error(dataSDM$new(coordinates = coordnames,
                            projection = projection,
@@ -107,7 +112,7 @@ test_that('dataSDMs initialize works as expected.', {
                            ips = iPoints,
                            spatial = FALSE,
                            intercepts = FALSE),
-               'INLAmesh needs to be an inla.mesh object.')
+               'Mesh needs to be an inla.mesh object.')
   
   ##Test coordinates error
   expect_error(dataSDM$new(coordinates = 'long',
@@ -169,7 +174,6 @@ test_that('addData can correctly add and store the relevent metadata properly.',
   expect_equal(check$.__enclos_env__$private$dataSource, c(rep('PO',3), rep('PA',2)))
   
   ##Check link functions
-  check$.__enclos_env__$private$optionsINLA
   expect_setequal(unlist(check$.__enclos_env__$private$optionsINLA), c('log', 'log', 'log', 'cloglog', 'cloglog'))
   ##Add a new counts dataset; first try without a species variable (compulsory since speciesName specified in the initialize...)
   Pcount <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
@@ -178,6 +182,7 @@ test_that('addData can correctly add and store the relevent metadata properly.',
   
   #Generate species
   Pcount$species <- 'dog'
+  Pcount$temp <- sample(c(1,2), nrow(Pcount@data), TRUE)
   check$addData(Pcount)
   ##Check that the data has been added into the model
   expect_setequal(names(check$.__enclos_env__$private$modelData), c("PO_fish_coordinates", "PO_fish_numvar", "PO_fish_factvar_response", "PA_bird_PAresp", "PA_bird_binommark", "Pcount_dog_count"))
@@ -187,39 +192,16 @@ test_that('addData can correctly add and store the relevent metadata properly.',
   PA2 <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
   PA2$newResponse <- sample(0:1, nrow(PA2@coords), replace = TRUE)
   PA2$species <- 'insect'
+  PA2$temp <- sample(c(1,2), nrow(PA2@data), TRUE)
   check$addData(PA2, responsePA = 'newResponse')
   
   expect_setequal(names(check$.__enclos_env__$private$modelData), c("PO_fish_coordinates", "PO_fish_numvar", "PO_fish_factvar_response", "PA_bird_PAresp", "PA_bird_binommark", "Pcount_dog_count", 'PA2_insect_PAresp'))
   expect_setequal(check$.__enclos_env__$private$dataSource, c("PO", "PO", "PO", "PA", "PA", "Pcount", "PA2"))
   ##The correct species_field is in the components, ie the object updated itself correctly
-  expect_true('species_spatial(main = coordinates, model = speciesModel, group = species, ngroup = 4)' %in% check$.__enclos_env__$private$Components)
+  expect_true('shared_spatial(main = coordinates, model = shared_field, group = temp, ngroup = 2, control.group = list(model = \"ar1\"))' %in% check$.__enclos_env__$private$Components)
   expect_equal(unlist(check$.__enclos_env__$private$speciesIn), c(PO = 'fish', PA = 'bird', Pcount = 'dog', PA2 = 'insect'))
   
   })
-
-#Make a random spatial covariate
-cov <- sp::spsample(x = SpatialPoly, n = 100000, type = 'random')
-cov$covariate <- rgamma(n = 100000, shape = 2)
-cov <- sp::SpatialPixelsDataFrame(points = cov@coords,
-                                  data = data.frame(covariate = cov$covariate),
-                                  proj4string = projection,
-                                  tolerance = 0.898631)
-
-test_that('spatialCovariates can add spatial covariates to the model and succesfully update the formulas and components of the model.', {
-  
-  check$spatialCovariates(cov)
-  
-  #Is the new covariate in the formulas
-  expect_true(all(unlist(lapply(check$.__enclos_env__$private$modelData, function(x) {
-    
-    'covariate'%in% x$include
-    
-  } ))))
-  
-  #covariate in components
-  expect_true("fish_covariate(main = covariate, model = \"linear\") + bird_covariate(main = covariate, model = \"linear\") + dog_covariate(main = covariate, model = \"linear\") + insect_covariate(main = covariate, model = \"linear\")" %in% check$.__enclos_env__$private$Components)
-  
-})
 
 test_that('addBias is able to add bias fields to the model as well as succesfully update the relevent formulas and components of the model.', {
   
@@ -233,32 +215,33 @@ test_that('addBias is able to add bias fields to the model as well as succesfull
   POdats <- grepl('^PO', names(check$.__enclos_env__$private$modelData))
   expect_true(all(unlist(lapply(check$.__enclos_env__$private$modelData[POdats], function(x) {
     
-    'PO_bias_field' %in% x$include
+    'PO_biasField' %in% x$include
     
   }))))
   expect_false(all(unlist(lapply(check$.__enclos_env__$private$modelData[!POdats], function(x) {
     
-    'PO_bias_field' %in% x$include
+    'PO_biasField' %in% x$include
     
   }))))
-  expect_true('PO_bias_field(main = coordinates, model = biasField)' %in% check$.__enclos_env__$private$Components)
+  expect_true('PO_biasField(main = coordinates, model = PO_bias_field)' %in% check$.__enclos_env__$private$Components)
   
-  expect_identical(check$.__enclos_env__$private$biasField, pcmatern)
+  expect_identical(check$spatialFields$biasFields$PO, pcmatern)
   
   #Change bias field
   
 })
 
-test_that('changeFormula is able to change the formula of a dataset', {
+test_that('updateFormula is able to change the formula of a dataset', {
   
   ##remove the covariate from the PO dataset
-  check$changeFormula('PO', formula = ~ ., keepSpatial = TRUE, keepIntercepts = TRUE)
+  check$updateFormula(datasetName = 'PO', Formula = ~ . - covariate)
   
   expect_setequal(check$.__enclos_env__$private$modelData$PO_fish_coordinates$include_components, c("shared_spatial", "species_spatial", "fish_intercept" ))
   expect_setequal(check$.__enclos_env__$private$modelData$PA_bird_PAresp$include_components, c("species_spatial", "shared_spatial", "bird_intercept", "pointcov", "covariate"))
   
   #remove covariate for the numvar mark
-  check$changeFormula('PO', markName = 'numvar', formula = ~ ., keepSpatial = TRUE, keepIntercepts = TRUE)
+  check$updateFormula('PO', markName = 'numvar', formula = ~ ., keepSpatial = TRUE, keepIntercepts = TRUE)
   expect_setequal(check$.__enclos_env__$private$modelData$PO_fish_numvar$include_components, c("shared_spatial", "species_spatial", "fish_intercept"))
   
 })
+
