f57fab69ba18306297c6fcde3d5a81c906a6fd9a
Philip Mostert <philism@dhcp-10-24-21-41.wlan.ntnu.no>
2022-03-14 12:57:50 +0100

    Fixed data objects

---
+++ R/data.SetophagaData.rda.R
@@ -0,0 +1,7 @@
+#' Data object containing the five heterogeneous datasets of species, setophaga, as well as environmental covariates (canopy and elevation) describing Pennsylvania state.
+#' 
+#' @name SetophagaData
+#' @docType data
+#' @keywords data
+#' @references \url{https://ebird.org/home}, \url{https://cran.r-project.org/web/packages/elevatr/index.html}, \url{https://cran.r-project.org/web/packages/FedData/index.html}.
+NULL
