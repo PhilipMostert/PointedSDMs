8d27817c7a9fa50f074351277725a63553d928ba
Philip Mostert <philism@dhcp-10-24-21-41.wlan.ntnu.no>
2022-03-14 13:06:58 +0100

    Updates to references

--- R/data.SetophagaData.rda.R
+++ R/data.SetophagaData.rda.R
@@ -1,7 +1,50 @@
-#' Data object containing the five heterogeneous datasets of species, setophaga, as well as environmental covariates (canopy and elevation) describing Pennsylvania state.
-#' 
-#' @name SetophagaData
+#' Raster object containing the elevation across Pennsylvania state.
+#' @name elev_raster
 #' @docType data
 #' @keywords data
-#' @references \url{https://ebird.org/home}, \url{https://cran.r-project.org/web/packages/elevatr/index.html}, \url{https://cran.r-project.org/web/packages/FedData/index.html}.
+#' @references \url{https://cran.r-project.org/web/packages/elevatr/index.html}
 NULL
+
+#' Raster object containing the canopy cover across Pennsylvania state.
+#' @name elev_raster
+#' @docType data
+#' @keywords data
+#' @references \url{https://cran.r-project.org/web/packages/FedData/index.html}
+NULL
+
+#' Dataset of setophaga caerulescens across Pennsylvania state.
+#' @name eBird_caerulescens
+#' @docType data
+#' @keywords data
+#' @references \url{https://ebird.org/home}.
+NULL
+
+#' Dataset of setophaga fusca across Pennsylvania state.
+#' @name eBird_fusca
+#' @docType data
+#' @keywords data
+#' @references \url{https://ebird.org/home}.
+NULL
+
+#' Dataset of setophaga magnolia across Pennsylvania state.
+#' @name eBird_magnolia
+#' @docType data
+#' @keywords data
+#' @references \url{https://ebird.org/home}.
+NULL
+
+#' Dataset of setophaga caerulescens obtained from the Pennsylvania Atlas of Breeding Birds.
+#' @name BBA
+#' @docType data
+#' @keywords data
+#' @references \url{http://www.pabirdatlas.psu.edu}.
+NULL
+
+
+#' Dataset of setophaga caerulescens obtained from the North American Breeding Bird survey across Pennsylvania state.
+#' @name BBS
+#' @docType data
+#' @keywords data
+#' @references \url{https://www.pwrc.usgs.gov/bbs/}.
+NULL
+
