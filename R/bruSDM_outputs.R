084c0c9a54bc3a621fd5b63e1209fe280f09b3cb
Philip Mostert <philism@dhcp-10-24-21-41.wlan.ntnu.no>
2022-03-14 12:26:43 +0100

    Updates to summary function

--- R/bruSDM_outputs.R
+++ R/bruSDM_outputs.R
@@ -23,7 +23,7 @@ print.bruSDM <- function(x, ...) {
 #' @rdname summary.bruSDM
 #' @exportS3Method 
 
-summary.bruSDM <- function(x,...) {
+summary.bruSDM <- function(x, ...) {
   #cat('----bru_sdm summary STILL IN DEVELOPMENT----\n\n')
   
   cat("Summary of 'bruSDM' object:\n\n")
@@ -73,7 +73,7 @@ summary.bruSDM <- function(x,...) {
       class(x) = 'inla'
       x$call = NULL
       x$summary.fixed = NULL
-      summary(x)
+      summary(x, ...)
       
   }
   
@@ -81,7 +81,7 @@ summary.bruSDM <- function(x,...) {
     
     class(x) = 'inla'
     x$call = NULL
-    summary(x)
+    summary(x, ...)
     
   }
   
