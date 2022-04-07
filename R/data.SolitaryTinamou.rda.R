cfcb84db1c0cb4e1c32c864f176f45239e14b7d3
Philip Mostert <philism@dhcp-10-24-23-113.wlan.ntnu.no>
2022-04-07 08:47:22 +0200

    Update description of solitary tinamou data

---
+++ R/data.SolitaryTinamou.rda.R
@@ -0,0 +1,50 @@
+#' data.frame object containing solitary tinamou observations from eBird
+#' @name eBird
+#' @docType data
+#' @keywords data
+#' @references \url{http://ebird.org/}
+NULL
+
+#' data.frame object containing solitary tinamou observations from Parks
+#' @name Parks
+#' @docType data
+#' @keywords data
+#' @source \url{http://www.gbif.org/}
+NULL
+
+#' data.frame object containing solitary tinamou observations from Gbif
+#' @name Gbif
+#' @docType data
+#' @keywords data
+#' @source \url{http://www.gbif.org/}
+NULL
+
+#' SpatialPixelsDataFrame containing Forest covariate
+#' @name Forest
+#' @docType data
+#' @keywords data
+#' @source \url{https://github.com/oharar/PointedSDMs}
+NULL
+
+#' SpatialPixelsDataFrame containing NPP covariate
+#' @name NPP
+#' @docType data
+#' @keywords data
+#' @source \url{https://github.com/oharar/PointedSDMs}
+NULL
+
+#' SpatialPixelsDataFrame containing Altitude covariate
+#' @name Altitude
+#' @docType data
+#' @keywords data
+#' @source \url{https://github.com/oharar/PointedSDMs}
+NULL
+
+#' SpatialPolygons object containing the boundary region for solitary tinamouc 
+#' @name region
+#' @docType data
+#' @keywords data
+#' @source \url{https://github.com/oharar/PointedSDMs}
+NULL
+
+
