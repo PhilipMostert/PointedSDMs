a03d9f121f5a0cb1cbddf15064c0f0014079a9b8
Philip Mostert <philism@dhcp-10-24-20-157.wlan.ntnu.no>
2022-05-09 12:49:45 +0200

    Removed arbitrary printing of fold_ind

--- R/blockedCV.R
+++ R/blockedCV.R
@@ -67,7 +67,7 @@ blockedCV <- function(data, options = list()) {
     foldOptions <- data$.__enclos_env__$private$optionsINLA
     
     fold_ind <- unique(unlist(block_index))[unique(unlist(block_index)) != fold]
-    print(fold_ind)
+    
     foldOptions$control.family <- foldOptions$control.family[sapply(unlist(data$.__enclos_env__$private$modelData), 
                                                                     function(x) any(fold_ind %in% x@data[, '.__block_index__']))]
 
