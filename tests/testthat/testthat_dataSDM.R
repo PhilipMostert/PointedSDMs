fbf81e90ce813253e78bc053b324823355bb50ea
Philip Mostert <philism@dhcp-10-24-20-125.wlan.ntnu.no>
2022-05-28 14:46:41 +0200

    Imports for INLA

--- tests/testthat/testthat_dataSDM.R
+++ tests/testthat/testthat_dataSDM.R
@@ -28,9 +28,9 @@ PA$binommark <- sample(x = 2:5, size = nrow(PA@data), replace = TRUE)
 PA$marktrial <- sample(x = 0:1, size = nrow(PA@data), replace = TRUE)
 PA$species <- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
 PA$temp <- sample(x = c(1,2), size = nrow(PA@coords), replace = TRUE)
-mesh <- INLA::inla.mesh.2d(boundary = inla.sp2segment(SpatialPoly), 
+mesh <- INLA::inla.mesh.2d(boundary = INLA::inla.sp2segment(SpatialPoly), 
                            max.edge = 2)
-iPoints <- inlabru::ipoints(samplers = SpatialPoly)
+iPoints <- inlabru::ipoints(samplers = SpatialPoly, domain = mesh)
 ##Make PA a data.frame object
 PA <- data.frame(PA)
 
