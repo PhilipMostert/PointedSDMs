401661d49f4fcfc73ab590e948f565f3df9752ab
Philip Mostert <philism@dhcp-10-24-20-125.wlan.ntnu.no>
2022-05-28 11:29:39 +0200

    Updates to testhat

--- tests/testthat/testthat_runModel.R
+++ tests/testthat/testthat_runModel.R
@@ -28,7 +28,7 @@ test_that('runModel runs a dataSDM object, and produces an INLA model with extra
   PA$binommark <- sample(x = 2:3, size = nrow(PA@data), replace = TRUE)
   PA$marktrial <- sample(x = 3:5, size = nrow(PA@data), replace = TRUE)
   PA$species <- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
-  mesh <- INLA::inla.mesh.2d(boundary = inla.sp2segment(SpatialPoly), 
+  mesh <- INLA::inla.mesh.2d(boundary = INLA::inla.sp2segment(SpatialPoly), 
                              max.edge = 2)
   iPoints <- inlabru::ipoints(samplers = SpatialPoly)
   ##Make PA a data.frame object
