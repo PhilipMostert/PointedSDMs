53cb1376c07a1b14fcdcc7f3cb28cb96cd58ad05
Philip Mostert <philism@dhcp-10-24-22-154.wlan.ntnu.no>
2022-06-08 14:50:39 +0200

    Moved all objects back out of testthat

--- tests/testthat/testthat_dataSDM.R
+++ tests/testthat/testthat_dataSDM.R
@@ -1,64 +1,70 @@
+projection <- CRS('+proj=tmerc')
+#Make random shape to generate points on
+x <- c(16.48438,  17.49512,  24.74609, 22.59277, 16.48438)
+y <- c(59.736328125, 55.1220703125, 55.0341796875, 61.142578125, 59.736328125)
+xy <- cbind(x, y)
+
+Poly <- Polygon(xy)
+Poly <- Polygons(list(Poly),1)
+SpatialPoly <- SpatialPolygons(list(Poly), proj4string = projection)
+
+##Old coordinate names
+#Make random points
+#Random presence only dataset
+PO <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
+##Add random variable
+PO$numvar <- runif(n = nrow(PO@coords))
+PO$factvar <- sample(x = c('a','b'), size = nrow(PO@coords), replace = TRUE)
+PO$species <- sample(x = c('fish'), size = nrow(PO@coords), replace = TRUE)
+PO$temp <- sample(x = c(1,2), size = nrow(PO@coords), replace = TRUE)
+#Random presence absence dataset
+PA <- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
+PA$PAresp <- sample(x = c(0,1), size = nrow(PA@coords), replace = TRUE)
+#Add trial name
+PA$trial <- sample(x = c(1,2,3), size = nrow(PA@coords), replace = TRUE)
+PA$pointcov <- runif(n = nrow(PA@coords))
+PA$binommark <- sample(x = 2:5, size = nrow(PA@data), replace = TRUE)
+PA$marktrial <- sample(x = 0:1, size = nrow(PA@data), replace = TRUE)
+PA$species <- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
+PA$temp <- sample(x = c(1,2), size = nrow(PA@coords), replace = TRUE)
+
+if (requireNamespace("INLA")) {
+mesh <<- INLA::inla.mesh.2d(boundary = INLA::inla.sp2segment(SpatialPoly), 
+                            max.edge = 2)
+iPoints <<- inlabru::ipoints(samplers = SpatialPoly, domain = mesh)
+}
+
+##Make PA a data.frame object
+PA <- data.frame(PA)
+
+coordnames <- colnames(PO@coords)
+responseCounts <- 'count'
+responsePA <- 'PAresp'
+trialName <- 'trial'
+markNames <- c('numvar', 'factvar', 'binommark')
+marksFamily <- c('gaussian', 'multinomial', 'binomial')
+markTrial <- 'marktrial'
+pointCovs <- 'pointcov'
+speciesName <- 'species'
+markSpatial <- TRUE
+marksIntercept <- TRUE
+speciesSpatial <- TRUE
+temporalName <- 'temp'
+temporalModel <- deparse(list(model = 'ar1'))
+projection <- CRS('+proj=tmerc')
+
+
+cov <- sp::spsample(x = SpatialPoly, n = 100000, type = 'random')
+cov$covariate <- rgamma(n = 100000, shape = 2)
+cov <- sp::SpatialPixelsDataFrame(points = cov@coords,
+                                   data = data.frame(covariate = cov$covariate),
+                                   proj4string = projection,
+                                   tolerance = 0.898631)
+
 test_that('dataSDMs initialize works as expected.', {
   
   skip_on_cran()
-  projection <- CRS('+proj=tmerc')
-  
-  #Make random shape to generate points on
-  x <- c(16.48438,  17.49512,  24.74609, 22.59277, 16.48438)
-  y <- c(59.736328125, 55.1220703125, 55.0341796875, 61.142578125, 59.736328125)
-  xy <- cbind(x, y)
-  
-  Poly <<- Polygon(xy)
-  Poly <<- Polygons(list(Poly),1)
-  SpatialPoly <<- SpatialPolygons(list(Poly), proj4string = projection)
-  
-  ##Old coordinate names
-  #Make random points
-  #Random presence only dataset
-  PO <<- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
-  ##Add random variable
-  PO$numvar <<- runif(n = nrow(PO@coords))
-  PO$factvar <<- sample(x = c('a','b'), size = nrow(PO@coords), replace = TRUE)
-  PO$species <<- sample(x = c('fish'), size = nrow(PO@coords), replace = TRUE)
-  PO$temp <<- sample(x = c(1,2), size = nrow(PO@coords), replace = TRUE)
-  #Random presence absence dataset
-  PA <<- spsample(SpatialPoly, n = 100, 'random', CRSobs = projection)
-  PA$PAresp <<- sample(x = c(0,1), size = nrow(PA@coords), replace = TRUE)
-  #Add trial name
-  PA$trial <<- sample(x = c(1,2,3), size = nrow(PA@coords), replace = TRUE)
-  PA$pointcov <<- runif(n = nrow(PA@coords))
-  PA$binommark <<- sample(x = 2:5, size = nrow(PA@data), replace = TRUE)
-  PA$marktrial <<- sample(x = 0:1, size = nrow(PA@data), replace = TRUE)
-  PA$species <<- sample(x = c('bird'), nrow(PA@data), replace = TRUE)
-  PA$temp <<- sample(x = c(1,2), size = nrow(PA@coords), replace = TRUE)
-  mesh <<- INLA::inla.mesh.2d(boundary = INLA::inla.sp2segment(SpatialPoly), 
-                             max.edge = 2)
-  iPoints <<- inlabru::ipoints(samplers = SpatialPoly, domain = mesh)
-  ##Make PA a data.frame object
-  PA <<- data.frame(PA)
-  
-  coordnames <- colnames(PO@coords)
-  responseCounts <- 'count'
-  responsePA <- 'PAresp'
-  trialName <- 'trial'
-  markNames <- c('numvar', 'factvar', 'binommark')
-  marksFamily <- c('gaussian', 'multinomial', 'binomial')
-  markTrial = 'marktrial'
-  pointCovs <- 'pointcov'
-  speciesName <- 'species'
-  markSpatial <- TRUE
-  marksIntercept <- TRUE
-  speciesSpatial <- TRUE
-  temporalName <- 'temp'
-  temporalModel <- deparse(list(model = 'ar1'))
-  
-  cov <<- sp::spsample(x = SpatialPoly, n = 100000, type = 'random')
-  cov$covariate <<- rgamma(n = 100000, shape = 2)
-  cov <<- sp::SpatialPixelsDataFrame(points = cov@coords,
-                                    data = data.frame(covariate = cov$covariate),
-                                    proj4string = projection,
-                                    tolerance = 0.898631)
-  
+
   check <<- dataSDM$new(coordinates = coordnames,
                        projection = projection,
                        Inlamesh = mesh, speciesspatial = speciesSpatial,
