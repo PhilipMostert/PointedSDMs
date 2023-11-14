0de1db71040446d4e1b7dc9059545f8964cf3a2b
Philip Mostert <philip.s.mostert@ntnu.no>
2023-11-14 16:01:07 +0100

    New argument for intModel:s speciesEffects. Allows the user to customize the species effects

--- R/intModel.R
+++ R/intModel.R
@@ -18,6 +18,7 @@
 #' @param Offset Name of the offset variable (class \code{character}) in the datasets. Defaults to \code{NULL}; if the argument is non-\code{NULL}, the variable name needs to be standardized across datasets (but does not need to be included in all datasets). The offset variable will be transformed onto the log-scale in the integrated model.
 #' @param pointsIntercept Logical argument: should the points be modeled with intercepts. Defaults to \code{TRUE}.
 #' @param marksIntercept Logical argument: should the marks be modeled with intercepts. Defaults to \code{TRUE}.
+#' @param speciesEffects List specifying if intercept terms and environmentsl effects should be made for the species. Defaults to \code{list(Intercept = TRUE, Environmental = TRUE)}. Note that if \code{Intercept = FALSE} and \code{pointsIntercept = TRUE}, dataset specific intercept terms will be created.
 #' @param pointsSpatial Argument to determine whether the spatial field is shared between the datasets, or if each dataset has its own unique spatial field. The datasets may share a spatial field with \pkg{INLA}'s "copy" feature if the argument is set to \code{copy}. May take on the values: \code{"shared"}, \code{"individual"}, \code{"copy"} or \code{NULL} if no spatial field is required for the model. Defaults to \code{"shared"}.
 #' @param marksSpatial Logical argument: should the marks have their own spatial field. Defaults to \code{TRUE}.
 #' @param responseCounts Name of the response variable in the counts/abundance datasets. This variable name needs to be standardized across all counts datasets used in the integrated model. Defaults to \code{'counts'}.
@@ -78,7 +79,9 @@ intModel <- function(..., spatialCovariates = NULL, Coordinates,
                      Boundary = NULL, speciesSpatial = 'copy',
                      speciesIndependent = FALSE,
                      markNames = NULL, markFamily = NULL,
-                     pointCovariates = NULL, pointsIntercept = TRUE, marksIntercept = TRUE,
+                     pointCovariates = NULL, 
+                     pointsIntercept = TRUE, marksIntercept = TRUE, 
+                     speciesEffects = list(Intercept = TRUE, Environmental = TRUE),
                      Offset = NULL, pointsSpatial = 'shared', marksSpatial = TRUE,
                      responseCounts = 'counts', responsePA = 'present', trialsPA = NULL,
                      trialsMarks = NULL, speciesName = NULL, temporalName = NULL,
@@ -170,6 +173,13 @@ intModel <- function(..., spatialCovariates = NULL, Coordinates,
   if (is.null(pointsSpatial) || pointsSpatial == 'shared') copyModel <- NULL
   else copyModel <- deparse1(copyModel)
   
+  if (!all(names(speciesEffects) %in% c('Intercept', 'Environmental'))) stop ('speciesEffects needs to be a named list with two items: Intercept and Environmental.')
+  
+  speciesIntercept <- speciesEffects$Intercept
+  speciesEnvironment <- speciesEffects$Environmental
+  
+  #if (!is.null(speciesName) && speciesIntercept) pointsIntercept <- FALSE
+  
   bruData <- dataSDM$new(coordinates = Coordinates, projection = Projection,
                          Inlamesh = Mesh, initialnames = initialnames,
                          responsecounts = responseCounts,
@@ -187,6 +197,8 @@ intModel <- function(..., spatialCovariates = NULL, Coordinates,
                          spatialcovariates = spatialCovariates,
                          boundary = Boundary,
                          ips = IPS,
+                         speciesenvironment = speciesEnvironment,
+                         speciesintercept = speciesIntercept,
                          temporal = temporalName,
                          temporalmodel = temporalModel,
                          speciesspatial = speciesSpatial,
