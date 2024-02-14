b9ea9ed403d479e89452e9b136799b10a1ba2141
Philip Mostert <philip.s.mostert@ntnu.no>
2024-02-14 09:24:37 +0100

    Only keep names of species in integration points

--- R/makeLhoods.R
+++ R/makeLhoods.R
@@ -71,6 +71,10 @@ makeLhoods <- function(data, formula, family, mesh, ips,
             speciesRep <- data.frame(rep(unique(data.frame(data[[dataset]][[species]])[,speciesname]), nrow(ips)))
             names(speciesRep) <- speciesname
             IPS <- ips
+            
+            namesKeep <- names(IPS)[names(IPS) %in% c('weight', '.block',names(data[[dataset]][[species]]))]
+            IPS <- IPS[, namesKeep]
+            
             IPS <- cbind(ips, speciesRep)
             
           }
