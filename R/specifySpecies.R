6ad5bce512854b965772e7a05271ab59f27d013b
Philip Mostert <philip.s.mostert@ntnu.no>
2024-08-24 09:07:58 +0200

    Improve covariate matching

--- R/specifySpecies.R
+++ R/specifySpecies.R
@@ -2039,11 +2039,3 @@ specifySpecies$set('public', 'spatialFields', list(sharedField = list(),
                                                 datasetFields = list(),
                                                 biasFields = list(),
                                                 speciesFields = list()))
-nearestValue <- function(pts, r,...) {
-  # Convert SpatRaster to points
-  rxy <- as.data.frame(r, xy = TRUE, na.rm = TRUE)
-  # Get the nearest raster cell index for each point
-  ind <- FNN::knnx.index(rxy[, c("x", "y")], pts, k = 1, ...)[, 1]
-  # Return the raster values corresponding to the nearest points
-  rxy[ind, 3]
-}
